e06c857c579c0c937acdb13482d5928357fb5054
Scott Luinski <cuda318cid@yahoo.com>
2015-02-08 22:48:56 -0500

    Working thing
    Made a thing that uses data from a method

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number to be doubled: ");   
+     
+            int usrNum = Convert.ToInt32(Console.ReadLine());     
+            Numm(usrNum);                                          
+            Console.WriteLine("Answer is: " + Numm(usrNum));     
+  
+            Console.ReadKey();                                     
+        }
+
+        public static int Numm(int numm)    
+        {
+            int timesTwo = numm * 2; 
+            return timesTwo;                 
+
+        }
+    }
+}
